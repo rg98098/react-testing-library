14a0e320bf39a021a1a5cf2b5d04be888c17988b
rg98098 <rg98098@gmail.com>
2022-07-21 14:19:52 +0530

    auto formatting

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,14 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import App, { replaceCamelWithSpaces } from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App, { replaceCamelWithSpaces } from "./App";
 
-test('button has correct initial color', () => {
+test("button has correct initial color", () => {
   render(<App />);
-  
+
   // // find an element with a role of button and text of 'Change to blue'
-  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
 
   // // expect the background color to be red
-  expect(colorButton).toHaveStyle({ backgroundColor: 'red' })
+  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
 
   // render(<App/>);
 
@@ -28,61 +28,61 @@ test('button has correct initial color', () => {
   // expect(colorButton.textContent).toBe('Change to red');
 });
 
-test('Have click on button to change its color to Red', ()=> {
-  render(<App/>)
+test("Have click on button to change its color to Red", () => {
+  render(<App />);
 
-  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
 
   fireEvent.click(colorButton);
-  expect(colorButton).toHaveStyle({ backgroundColor: 'blue'});
-  expect(colorButton.textContent).toBe('Change to red');
-})
+  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
+  expect(colorButton.textContent).toBe("Change to red");
+});
 
-test('initial conditions', ()=> {
-  render(<App />)
-  const colorButton = screen.getByRole('button', { name: 'Change to blue'});
+test("initial conditions", () => {
+  render(<App />);
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
   expect(colorButton).toBeEnabled();
 
-  const checkbox = screen.getByRole('checkbox');
+  const checkbox = screen.getByRole("checkbox");
 
   expect(checkbox).not.toBeChecked();
-})
+});
 
-test('when checkbox is checked, button should be disabled', ()=> {
-  render(<App />)
+test("when checkbox is checked, button should be disabled", () => {
+  render(<App />);
 
-  const checkbox = screen.getByRole('checkbox', { name: 'Disable button'});
-  const colorButton = screen.getByRole('button', { name: 'Change to blue'});
-  fireEvent.click(checkbox)
-  expect(colorButton).toBeDisabled();
-  
-  fireEvent.click(checkbox)
-  expect(colorButton).toBeEnabled();
-})
-
-test('button gray when disabled', ()=> {
-  render(<App />)
-  const checkbox = screen.getByRole('checkbox', { name: 'Disable button'})
-  const colorButton = screen.getByRole('button', { name: 'Change to blue'})
-  fireEvent.click(checkbox)
-  expect(colorButton).toHaveStyle({ backgroundColor: 'gray'});
+  const checkbox = screen.getByRole("checkbox", { name: "Disable button" });
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
   fireEvent.click(checkbox);
-  expect(colorButton).toHaveStyle({ backgroundColor: 'red'});
+  expect(colorButton).toBeDisabled();
+
+  fireEvent.click(checkbox);
+  expect(colorButton).toBeEnabled();
+});
+
+test("button gray when disabled", () => {
+  render(<App />);
+  const checkbox = screen.getByRole("checkbox", { name: "Disable button" });
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+  fireEvent.click(checkbox);
+  expect(colorButton).toHaveStyle({ backgroundColor: "gray" });
+  fireEvent.click(checkbox);
+  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
   fireEvent.click(colorButton);
   fireEvent.click(checkbox);
-  expect(colorButton).toHaveStyle({ backgroundColor: 'gray'});
+  expect(colorButton).toHaveStyle({ backgroundColor: "gray" });
   fireEvent.click(checkbox);
-  expect(colorButton).toHaveStyle({ backgroundColor: 'blue'});
-})
+  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
+});
 
-describe('spaces before camel-case capital letters', () => {
-  test('Work for no inner capital letters', () => {
-    expect(replaceCamelWithSpaces('Red')).toBe('Red');
-  })
-  test('Work for one inner capital letters', () => {
-    expect(replaceCamelWithSpaces('MidnightBlue')).toBe('Midnight Blue');
-  })
-  test('Work for multiple inner capital letters', () => {
-    expect(replaceCamelWithSpaces('MediumVioletRed')).toBe('Medium Violet Red');
-  })
-})
+describe("spaces before camel-case capital letters", () => {
+  test("Work for no inner capital letters", () => {
+    expect(replaceCamelWithSpaces("Red")).toBe("Red");
+  });
+  test("Work for one inner capital letters", () => {
+    expect(replaceCamelWithSpaces("MidnightBlue")).toBe("Midnight Blue");
+  });
+  test("Work for multiple inner capital letters", () => {
+    expect(replaceCamelWithSpaces("MediumVioletRed")).toBe("Medium Violet Red");
+  });
+});
